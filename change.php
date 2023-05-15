b96b935d518581f2bf1f1904eba89294ce92e06a
StonePlex <miha.safranko@gmail.com>
2023-05-15 10:17:40 +0200

    ss

--- change.php
+++ change.php
@@ -37,8 +37,37 @@ if ($query > 0) {
             $rezervacija_id = mysqli_insert_id($conn);
             $update_sql2 = "UPDATE zivali SET rezervacija_id = ".$rezervacija_id." WHERE id = ".$zival.";";
             if ($conn->query($update_sql2) === TRUE) {
-                setcookie('prijava', "Sprememba uspešna.");
-                header('Location: admin.php');
+                if ($_FILES['slika']['error'] === UPLOAD_ERR_OK) {
+                    $image_dir = 'img/';
+                    $image_name = $_FILES['slika']['name'];
+                    $image_tmp = $_FILES['slika']['tmp_name'];
+                    $image_path = $image_dir . $image_name;
+                
+                    // Move the uploaded file to the desired directory
+                    if (move_uploaded_file($image_tmp, $image_path)) {
+                        // Image upload successful
+                        echo "Image URL: " . $image_path;
+                        $slika_sql = "INSERT INTO slike (url) VALUES ('".$image_path."');";
+                        if ($conn->query($slika_sql) === TRUE){
+                            $slika_id = mysqli_insert_id($conn);
+                            $update_sql3 = "UPDATE zivali SET slika_id = ".$slika_id." id = ".$zival.";";
+                            if ($conn->query($update_sql3) === TRUE) {
+                                setcookie('prijava', "Sprememba uspešna.");
+                                header('Location: admin.php');
+                        }
+                        else{
+                            setcookie('prijava', "Error: " . $update_sql . "<br>" . $conn->error);
+                            header('Location: admin.php');
+                        }
+
+                    } else {
+                        // Error in moving the uploaded file
+                        echo "Error uploading the image.";
+                    }
+                } else {
+                    // Error in uploading the file
+                    echo "Error: " . $_FILES['slika']['error'];
+                }
             } else {
                 setcookie('prijava', "Error: " . $update_sql . "<br>" . $conn->error);
                 header('Location: admin.php');
@@ -53,5 +82,6 @@ if ($query > 0) {
     }
 } else {
     header('Location: index.php');
+}
 }
 ?>
