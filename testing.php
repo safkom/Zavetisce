20ec060cfee3ed68c4d282adc5f782e426902908
StonePlex <miha.safranko@gmail.com>
2023-05-17 12:22:31 +0200

    ss

--- testing.php
+++ testing.php
@@ -1,10 +1,10 @@
 <?php
-$target_dir = "img/";
+$target_dir = "/home/safkoeu/public_html/zavetisce/img/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
-if(isset($_GET["submit"])) {
+if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
     echo "File is an image - " . $check["mime"] . ".";
