413ddcb66b3d3fd22ca8f33cf532ff8fec8f4673
StonePlex <miha.safranko@gmail.com>
2023-05-17 12:36:55 +0200

    test?

--- change.php
+++ change.php
@@ -1,4 +1,9 @@
 <?php
+/*
+
+*/
+
+
 require_once 'connect.php';
 require_once 'cookie.php';
 
@@ -35,17 +40,30 @@ if ($query > 0) {
             $rezervacija_id = mysqli_insert_id($conn);
             $update_sql2 = "UPDATE zivali SET rezervacija_id = ".$rezervacija_id." WHERE id = ".$zival.";";
             if ($conn->query($update_sql2) === TRUE) {
-                if ($_FILES['slika']['error'] === UPLOAD_ERR_OK) {
-                    $image_dir = 'img/';
-                    $image_name = $_FILES['slika']['name'];
-                    $image_tmp = $_FILES['slika']['tmp_name'];
-                    $image_path = $image_dir . $image_name;
-
-                    // Move the uploaded file to the desired directory
-                    if (move_uploaded_file($image_tmp, $image_path)) {
-                        // Image upload successful
-                        echo "Image URL: " . $image_path;
-                        $slika_sql = "INSERT INTO slike (url) VALUES ('".$image_path."');";
+                if (isset($_POST['uploadBtn']) && $_POST['uploadBtn'] == 'Upload')
+{
+  if (isset($_FILES['uploadedFile']) && $_FILES['uploadedFile']['error'] === UPLOAD_ERR_OK)
+  {
+    // get details of the uploaded file 
+    $fileTmpPath = $_FILES['uploadedFile']['tmp_name'];
+    $fileName = $_FILES['uploadedFile']['name'];
+    $fileSize = $_FILES['uploadedFile']['size'];
+    $fileType = $_FILES['uploadedFile']['type'];
+    $fileNameCmps = explode(".", $fileName);
+    $fileExtension = strtolower(end($fileNameCmps));
+    // sanitize file-name 
+    $newFileName = md5(time() . $fileName) . '.' . $fileExtension;
+    // check if file has one of the following extensions 
+    $allowedfileExtensions = array('jpg', 'gif', 'png', 'zip', 'txt', 'xls', 'doc');
+    if (in_array($fileExtension, $allowedfileExtensions))
+    {
+      // directory in which the uploaded file will be moved 
+      $uploadFileDir = './img/';
+      $dest_path = $uploadFileDir . $newFileName;
+      if(move_uploaded_file($fileTmpPath, $dest_path)) 
+      {
+        setcookie('prijava','File is successfully uploaded.');
+        $slika_sql = "INSERT INTO slike (url) VALUES ('".$dest_path."');";
                         if ($conn->query($slika_sql) === TRUE) {
                             $slika_id = mysqli_insert_id($conn);
                             $update_sql3 = "UPDATE zivali SET slika_id = ".$slika_id." WHERE id = ".$zival.";";
@@ -58,7 +76,27 @@ if ($query > 0) {
                                 header('Location: admin.php');
                                 exit();
                             }
-                        } else {
+                        }
+      }
+      else 
+      {
+        setcookie('prijava','There was some error moving the file to upload directory. Please make sure the upload directory is writable by web server.');
+        header('Loaction: admin.php');
+    }
+
+    }
+    else
+    {
+        setcookie('prijava', 'Upload failed. Allowed file types: ' . implode(',', $allowedfileExtensions));
+        header('Loaction: admin.php');
+    }
+  }
+  else
+  {
+    setcookie('prijava','There is some error in the file upload. Please check the following error' . $_FILES['uploadedFile']['error']);
+  }
+}
+                         else {
                             setcookie('prijava', "Error: " . $conn->error);
                             header('Location: admin.php');
                             exit();
@@ -83,10 +121,4 @@ if ($query > 0) {
             header('Location: admin.php');
             exit();
         }
-    } else {
-        setcookie('prijava', "Error: " . $conn->error);
-        header('Location: index.php');
-        exit();
-    }
-}
 ?>
