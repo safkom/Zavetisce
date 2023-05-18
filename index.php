a2a50459c4063baf886484e78e2b4262adafdaa2
StonePlex <miha.safranko@gmail.com>
2023-05-18 16:28:29 +0200

    asd

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Miha Šafranko"/>
 	<meta name="author" content="Miha Šafranko" />
+    <link rel="stylesheet" href="login.css">
+
     <title>Zavetišče</title>
 </head>
 <body>
