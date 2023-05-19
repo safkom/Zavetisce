befe53050440ca9b48a88988b20497b11e043fab
StonePlex <miha.safranko@gmail.com>
2023-05-19 12:21:01 +0200

    retard sem ker ne znam js

--- main.php
+++ main.php
@@ -121,7 +121,35 @@ echo "Seznam kužkov:";
             echo $_COOKIE['prijava'];
         }?>
     </div>
-    <script src="js/script.js"></script>
+    
 <a href = "odjava.php">Odjava</a>
+<script>
+        // Check if the cookie 'prijava' exists
+        function checkCookie() {
+            var name = "prijava=";
+            var decodedCookie = decodeURIComponent(document.cookie);
+            var cookieArray = decodedCookie.split(';');
+            
+            for (var i = 0; i < cookieArray.length; i++) {
+                var cookie = cookieArray[i];
+                while (cookie.charAt(0) == ' ') {
+                    cookie = cookie.substring(1);
+                }
+                if (cookie.indexOf(name) == 0) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // Show the login window if the cookie exists
+        if (checkCookie()) {
+            var loginWindow = document.getElementById("loginWindow");
+            loginWindow.style.display = "block";
+            setTimeout(function() {
+                loginWindow.style.display = "none";
+            }, 5000);
+        }
+    </script>
 </body>
 </html>
