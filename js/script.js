395a526044ed4a7fa509a18a4987caeb08c054e6
StonePlex <miha.safranko@gmail.com>
2023-05-19 12:07:43 +0200

    asdasdas

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 function showPopup() {
     var popup = document.getElementById('infoPopup');
-    var content = document.getElementById('popupContent');
     popup.style.display = 'block';
-
     setTimeout(function() {
         popup.style.opacity = '0';
         setTimeout(function() {
