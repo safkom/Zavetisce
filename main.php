546580ea1d2119f39028d53b13818cd824d9955d
StonePlex <miha.safranko@gmail.com>
2023-05-19 12:22:58 +0200

    okey

--- main.php
+++ main.php
@@ -42,7 +42,7 @@ if($query == 0){
     header('Location: index.php');
     exit();
 }
-setcookie('prijava', '');
+setcookie('prijava', 'ok');
 $sql = "SELECT * FROM zivali;";
 $result = mysqli_query($conn,$sql);
 echo "<a href='rezervacije.php'>Rezervacije</a><br>";
