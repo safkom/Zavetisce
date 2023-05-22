ffb982e12204312b6ec7924e20bec4f23f5ba384
StonePlex <miha.safranko@gmail.com>
2023-05-22 10:51:42 +0200

    asd

--- spremembe.php
+++ spremembe.php
@@ -18,17 +18,34 @@
   $sql = "SELECT * from zivali WHERE id = ".$_GET['zival_id'].";";
   $result = mysqli_query($conn, $sql);
   $row = mysqli_fetch_array($result)?>
- <form id = "spremembe" action="change.php" method="post" enctype="multipart/form-data">
-    <label for="ime">Ime:</label><br>
-  <input type="text" id="ime" name="ime" value = "<?php echo $row['ime']?>" required><br>
-  <label for="datum">Datum rojstva:</label><br>
-  <input type="date" id="datum" name="datum" value = "<?php echo $row['datum_r']?>" required><br>
-  <label for="posvojen">Posvojen:</label><br>
-  <input type="checkbox" id="posvojen" name="posvojen" <?php if($row['posvojen']== 1){echo "checked";}?>><br>
-  <label for="slika">Slika:</label><br>
-  <input type="file" id="slika" name="slika"><br>
-  <label for="rezervacija">Rezervacija:</label><br>
-  <input type="datalist" id="uporabnikid" name="uporabnikid" value="<?php
+<div class="container">
+	<div class="screen">
+		<div class="screen__content">
+			<form class="login" id = "spremembe" action="change.php" method="post" enctype="multipart/form-data">
+				<div class="login__field">
+					<i class="login__icon fas fa-user"></i>
+					<label for="ime">Ime:</label><br>
+                    <input type="text" id="ime" name="ime" value = "<?php echo $row['ime']?>" required><br>
+				</div>
+				<div class="login__field">
+					<i class="login__icon fas fa-lock"></i>
+					<label for="datum">Datum rojstva:</label><br>
+                    <input type="date" id="datum" name="datum" class = login_input value = "<?php echo $row['datum_r']?>" required><br>
+				</div>
+                <div class="login__field">
+					<i class="login__icon fas fa-lock"></i>
+					<label for="posvojen">Posvojen:</label><br>
+                    <input type="checkbox" id="posvojen" name="posvojen" class="login_input" <?php if($row['posvojen']== 1){echo "checked";}?>><br>
+				</div>
+                <div class="login__field">
+					<i class="login__icon fas fa-lock"></i>
+					<label for="slika">Slika:</label><br>
+                    <input type="file" id="slika" class="login_input" name="slika"><br>
+				</div>
+                <div class="login__field">
+					<i class="login__icon fas fa-lock"></i>
+					<label for="rezervacija">Rezervacija:</label><br>
+  <input type="datalist" id="uporabnikid" class="login_input" name="uporabnikid" value="<?php
     $sql = "SELECT * from rezervacija WHERE zival_id = ".$_GET['zival_id'].";";
     $result = mysqli_query($conn, $sql);
     $rows = mysqli_num_rows($result);
@@ -77,18 +94,18 @@
     setcookie('register', '');
     ?>
 </datalist>
-  <input type="submit" name="uploadBtn" value="Pošlji">
-</form>
-<a href = "admin.php">Nazaj</a>
-<script>
-    document.getElementById("spremembe").addEventListener("submit", function() {
-      var checkbox = document.getElementById("posvojen");
-      if (checkbox.checked) {
-        checkbox.value = "1";
-      } else {
-        checkbox.value = "0";
-      }
-    });
-  </script>
-</body>
-</html>
+				</div>
+				<button class="button login__submit">
+					<span class="button__text">Pošlji</span>
+					<i class="button__icon fas fa-chevron-right"></i>
+				</button>				
+			</form>
+		</div>
+		<div class="screen__background">
+			<span class="screen__background__shape screen__background__shape4"></span>
+			<span class="screen__background__shape screen__background__shape3"></span>		
+			<span class="screen__background__shape screen__background__shape2"></span>
+			<span class="screen__background__shape screen__background__shape1"></span>
+		</div>		
+	</div>
+</div>
