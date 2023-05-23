96951ef4908060d031bfc2aba5a606fe4f1bed38
StonePlex <miha.safranko@gmail.com>
2023-05-23 10:57:45 +0200

    asd

--- novo.php
+++ novo.php
@@ -4,15 +4,22 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once 'connect.php';
 require_once 'cookie.php';
+
 $ime = $_POST['ime'];
 $date = $_POST['datum'];
 $posvojen = isset($_POST['posvojen']) ? 1 : 0;
+
 echo $ime, $date, $posvojen;
 
 // Update the zivali table
-$insert_sql = "INSERT INTO zivali (ime, datum_r, posvojen) VALUES ('".$ime."', '".$date."', '".$posvojen."');";
-if ($conn->query($insert_sql) === TRUE) {
+$insert_sql = "INSERT INTO zivali (ime, datum_r, posvojen) VALUES (?, ?, ?)";
+$stmt = $conn->prepare($insert_sql);
+$stmt->bind_param('ssi', $ime, $date, $posvojen);
+
+if ($stmt->execute()) {
+    $zival_id = $stmt->insert_id;
     echo "Vnos uspešen!";
+
     if (isset($_FILES['slika']) && $_FILES['slika']['error'] === UPLOAD_ERR_OK) {
         $fileTmpPath = $_FILES['slika']['tmp_name'];
         $fileName = $_FILES['slika']['name'];
@@ -22,6 +29,7 @@ if ($conn->query($insert_sql) === TRUE) {
         $fileExtension = strtolower(end($fileNameCmps));
 
         $allowedfileExtensions = array('jpg', 'gif', 'png');
+
         if (in_array($fileExtension, $allowedfileExtensions)) {
             $uploadFileDir = 'img/';
             $newFileName = md5(time() . $fileName) . '.' . $fileExtension;
@@ -33,33 +41,31 @@ if ($conn->query($insert_sql) === TRUE) {
                     // Update the slika_id in the zivali table
                     $update_sql3 = "UPDATE zivali SET slika_id = ? WHERE id = ?";
                     $stmt4 = $conn->prepare($update_sql3);
-                    $stmt4->bind_param('ii', $slika_id, $zival);
+                    $stmt4->bind_param('ii', $slika_id, $zival_id);
                     if ($stmt4->execute()) {
                         echo "Uspešen vnos slike";
                     } else {
                         echo "Vnos ni uspel";
-                    
+                    }
+                } else {
+                    echo "Vnos ni uspel";
                 }
-            } 
             } else {
-                echo 'There was some error moving the file to upload directory. Please make sure the upload directory is writable by the web server.';
-                
+                echo 'There was some error moving the file to the upload directory. Please make sure the upload directory is writable by the web server.';
             }
         } else {
             echo 'Upload failed. Allowed file types: ' . implode(',', $allowedfileExtensions);
         }
-} 
-else {
-    echo "Vnos brez slike uspešen";
-}
-}
-else{
+    } else {
+        echo "Vnos brez slike uspešen";
+    }
+} else {
     echo "ne dela";
 }
 
-
 // Function to insert a new slika record and return the inserted ID
-function insertSlika($conn, $url) {
+function insertSlika($conn, $url)
+{
     $slika_sql = "INSERT INTO slike (url) VALUES (?)";
     $stmt = $conn->prepare($slika_sql);
     $stmt->bind_param('s', $url);
