f4409b3d8cc62acf173071f665123044fcabebe2
StonePlex <miha.safranko@gmail.com>
2023-05-23 10:59:19 +0200

    s

--- novoform.php
+++ novoform.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Miha Šafranko"/>
 	<meta name="author" content="Miha Šafranko" />
-  <link rel="stylesheet" type="text/css" href="css/spremembe.css">
 
     <title>Vnos</title>
 </head>
