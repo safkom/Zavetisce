16ac764c49084867543079cd41ccdb2f1a169c33
StonePlex <miha.safranko@gmail.com>
2023-05-23 10:47:56 +0200

    asd

--- novo.php
+++ novo.php
@@ -54,7 +54,7 @@ if ($conn->query($insert_sql) === TRUE) {
         }
 } 
 else {
-    echo 'register', "Error: " . $sql . "<br>" . $conn->error;
+    echo 'register', "Error: " . $insert_sql . "<br>" . $conn->error;
 }
 }
 else{
