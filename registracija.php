5051c03c0200971ecc1bbfa3a9fb10e385a455ad
Miha Šafranko <miha.safranko@gmail.com>
2023-05-29 21:25:24 +0200

    sd

--- registracija.php
+++ registracija.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="sl">
 <head>
@@ -6,12 +5,82 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Miha Šafranko"/>
 	<meta name="author" content="Miha Šafranko" />
-    <title>Zavetišče</title>
+    <link rel="stylesheet" href="css/login.css">
     <style>
         body{
             background: linear-gradient(90deg, #C7C5F4, #776BCC);
         }
+        /* Global Styles */
+body {
+    font-family: Arial, sans-serif;
+    background: linear-gradient(90deg, #C7C5F4, #776BCC);	
+    margin: 0;
+    padding: 0;
+  }
+  
+  .container {
+  max-width: 400px;
+  margin: 50px auto;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 10px; /* Adding rounded corners */
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2); /* Increasing the box shadow */
+}
+
+
+  
+  h1 {
+    text-align: center;
+    color: #333;
+  }
+  
+  form {
+    margin-top: 20px;
+  }
+  
+  label {
+    display: block;
+    margin-bottom: 5px;
+    color: #555;
+  }
+  
+  .container input[type="text"],
+.container input[type="password"] {
+  width: 100%;
+  box-sizing: border-box;
+}
+
+.container input[type="submit"] {
+  width: 100%;
+  padding: 10px;
+  background-color: blue;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.container input[type="submit"]:hover {
+  background-color: lightblue;
+}
+  
+  a {
+    color: lightblue;
+    text-decoration: none;
+  }
+  
+  /* Responsive Styles */
+  @media (max-width: 600px) {
+    .container {
+      max-width: 100%;
+      margin: 20px;
+      box-shadow: none;
+    }
+  }
+  
     </style>
+
+    <title>Zavetišče</title>
 </head>
 <body>
   <?php
@@ -20,6 +89,7 @@
       echo $_COOKIE['register'];
   }
   ?>
+  <div class = "container">
  <form action="register.php" method="get">
     <label for="ime">Ime:</label><br>
   <input type="text" id="ime" name="ime" required><br>
@@ -47,6 +117,6 @@
   <input type="submit" value="Pošlji">
 </form>
 <p>Ste že uporabnik? <a href = "index.php">Pojdite na prijavo</a>
-
+  </div>
 </body>
 </html>
