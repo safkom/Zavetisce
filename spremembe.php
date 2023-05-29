5e96511f1befac86ccfd4c2dff7fa79effa756e6
Miha Šafranko <miha.safranko@gmail.com>
2023-05-29 22:59:11 +0200

    asdasd

--- spremembe.php
+++ spremembe.php
@@ -19,15 +19,50 @@
   border-radius: 10px; /* Adding rounded corners */
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.2); /* Increasing the box shadow */
 }
-        .container input[type="submit"] {
-            width: 100%;
-            padding: 10px;
-            background-color: blue;
-            color: #fff;  
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
+
+
+  
+  h1 {
+    text-align: center;
+    color: #333;
+  }
+  
+  form {
+    margin-top: 20px;
+  }
+  
+  label {
+    display: block;
+    margin-bottom: 5px;
+    color: #555;
+  }
+  
+  .container input[type="text"],
+.container input[type="password"] {
+  width: 100%;
+  box-sizing: border-box;
 }
+
+.container input[type="submit"] {
+  width: 100%;
+  padding: 10px;
+  background-color: blue;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.container input[type="submit"]:hover {
+  background-color: lightblue;
+}/* Responsive Styles */
+@media (max-width: 600px) {
+    .container {
+      max-width: 100%;
+      margin: 20px;
+      box-shadow: none;
+    }
+  }
         </style>
 </head>
 <body>
