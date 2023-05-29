5de3ac5f3168664749ccea1217a4cadecc883245
Miha Šafranko <miha.safranko@gmail.com>
2023-05-29 22:43:05 +0200

    asd

--- spremembe.php
+++ spremembe.php
@@ -7,6 +7,91 @@
 	<meta name="author" content="Miha Šafranko" />
   <link rel="stylesheet" type="text/css" href="css/spremembe.css">
     <title>Spremembe</title>
+    <style>
+
+        
+body{
+            background: linear-gradient(90deg, #C7C5F4, #776BCC);
+        }
+        /* Global Styles */
+body {
+    font-family: Arial, sans-serif;
+    background: linear-gradient(90deg, #C7C5F4, #776BCC);	
+    margin: 0;
+    padding: 0;
+  }
+  
+  .container {
+  max-width: 400px;
+  margin: 50px auto;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 10px; /* Adding rounded corners */
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2); /* Increasing the box shadow */
+}
+
+
+  
+  h1 {
+    text-align: center;
+    color: #333;
+  }
+  
+  form {
+    margin-top: 20px;
+  }
+  
+  label {
+    display: block;
+    margin-bottom: 5px;
+    color: #555;
+  }
+  
+  .container input[type="text"],
+.container input[type="password"] {
+  width: 100%;
+  box-sizing: border-box;
+}
+
+.container input[type="submit"] {
+  width: 100%;
+  padding: 10px;
+  background-color: blue;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.container input[type="submit"]:hover {
+  background-color: lightblue;
+}
+#loginWindow {
+    position: fixed;
+    bottom: 10px;
+    right: 10px;
+    width: 200px;
+    height: 100px;
+    background-color: lightgreen;
+    display: none;
+    padding: 10px;
+    border-radius: 5px;
+}
+  
+  a {
+    color: lightblue;
+    text-decoration: none;
+  }
+  
+  /* Responsive Styles */
+  @media (max-width: 600px) {
+    .container {
+      max-width: 100%;
+      margin: 20px;
+      box-shadow: none;
+    }
+  }
+        </style>
 </head>
 <body>
   <?php
