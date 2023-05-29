fdf1603f72f66db795a8b1063accd78c1affe677
Miha Šafranko <miha.safranko@gmail.com>
2023-05-29 21:59:07 +0200

    asd

--- main.php
+++ main.php
@@ -8,210 +8,194 @@
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <title>Zavetišče</title>
     <style>
-        body{
+        body {
             background: linear-gradient(90deg, #C7C5F4, #776BCC);
         }
-        
+
         #container {
             margin: 50px auto;
             padding: 20px;
             background-color: #fff;
-            border-radius: 10px; /* Adding rounded corners */
-            box-shadow: 0 0 20px rgba(0, 0, 0, 0.2); /* Increasing the box shadow */
-        }
-        
-        .menu {
-            display: inline-block;
-            cursor: pointer;
-            position: fixed;
-            top: 10px;
-            right: 10px;
-            z-index: 999;
-        }
-        
-        .bar1,
-        .bar2,
-        .bar3 {
-            width: 35px;
-            height: 5px;
-            background-color: #333;
-            margin: 6px 0;
-            transition: 0.4s;
-        }
-        
-        .change .bar1 {
-            transform: translate(0, 11px) rotate(-45deg);
-        }
-        
-        .change .bar2 {
-            opacity: 0;
-        }
-        
-        .change .bar3 {
-            transform: translate(0, -11px) rotate(45deg);
-        }
-        
-        .dropdown {
-            position: absolute;
-            top: 40px;
-            right: 0;
-            background-color: #f9f9f9;
-            min-width: 160px;
-            box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-            z-index: 999;
-            display: none;
-        }
-        
-        .dropdown-content {
-            padding: 12px 16px;
-        }
-        
-        .dropdown:hover .dropdown-content {
-            display: block;
+            border-radius: 10px;
+            /* Adding rounded corners */
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+            /* Increasing the box shadow */
         }
     </style>
 </head>
 
 <body>
-<!-- User menu button -->
-<div class="menu" onclick="myFunction(this)">
-    <div class="bar1"></div>
-    <div class="bar2"></div>
-    <div class="bar3"></div>
-    <div class="dropdown">
-        <div class="dropdown-content">
-            <a href="rezervacije.php">Rezervacije</a>
-            <a href="odjava.php">Odjava</a>
-        </div>
+    <?php
+    require_once 'cookie.php';
+    require_once 'connect.php';
+
+    if (!isset($_COOKIE['id'])) {
+        header('Location: index.php');
+        exit();
+    }
+
+    if (isset($_COOKIE['admin'])) {
+        header('Location: admin.php');
+        exit();
+    }
+
+    $id = mysqli_real_escape_string($conn, $_COOKIE['id']);
+
+    $sql = "SELECT * FROM uporabniki WHERE id = '$id';";
+    $result = mysqli_query($conn, $sql);
+    $query = mysqli_num_rows($result);
+
+    // Modify the if statement to check if id exists in the database
+    if ($query == 0) {
+        header('Location: index.php');
+        exit();
+    }
+
+    $sql = "SELECT * FROM zivali;";
+    $result = mysqli_query($conn, $sql);
+    ?>
+
+    <div class="menu" onclick="myFunction(this)">
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
     </div>
-</div>
 
-<div id="container">
-    <p>Seznam kužkov:</p>
-    <table border="1">
-        <tr>
-            <td><b>Ime</b></td>
-            <td><b>Starost</b></td>
-            <td><b>Posvojen</b></td>
-            <td><b>Slika</b></td>
-            <td><b>Rezerviraj</b></td>
-        </tr>
-        <?php
-        while ($row = mysqli_fetch_array($result)) {
-            $slikaid = $row['slika_id'];
-            $sql1 = "SELECT * FROM slike WHERE id = '$slikaid';";
-            $klic = mysqli_query($conn, $sql1);
-            $klic1 = mysqli_fetch_array($klic);
+    <div id="dropdown-menu" style="display: none;">
+        <a href="rezervacije.php">Rezervacije</a>
+        <a href="odjava.php">Odjava</a>
+    </div>
 
-            if ($klic1 !== null) {
-                $slika = $klic1['url'];
-            } else {
-                $slika = null;
+    <div id="container">
+        <p>Seznam kužkov:</p>
+        <table border="1">
+            <tr>
+                <td><b>Ime</b></td>
+                <td><b>Starost</b></td>
+                <td><b>Posvojen</b></td>
+                <td><b>Slika</b></td>
+                <td><b>Rezerviraj</b></td>
+            </tr>
+            <?php
+            while ($row = mysqli_fetch_array($result)) {
+                $slikaid = $row['slika_id'];
+                $sql1 = "SELECT * FROM slike WHERE id = '$slikaid';";
+                $klic = mysqli_query($conn, $sql1);
+                $klic1 = mysqli_fetch_array($klic);
+
+                if ($klic1 !== null) {
+                    $slika = $klic1['url'];
+                } else {
+                    $slika = null;
+                }
+
+                $dateOfBirth = $row['datum_r'];
+                $today = date("Y-m-d");
+                $diff = date_diff(date_create($dateOfBirth), date_create($today));
+                $ageInMonths = $diff->format('%m');
+                $ageInYears = $diff->format('%y');
+
+                if ($ageInYears == 1) {
+                    $leta = $ageInYears . ' leto in ';
+                } elseif ($ageInYears > 1 && $ageInYears < 5) {
+                    $leta = $ageInYears . ' leti in ';
+                } elseif ($ageInYears >= 5) {
+                    $leta = $ageInYears . ' let in ';
+                } else {
+                    $leta = '';
+                }
+
+                if ($ageInMonths == 1) {
+                    $age = $leta . '1 mesec';
+                } elseif ($ageInMonths > 1 && $ageInMonths < 5) {
+                    $age = $leta . $ageInMonths . ' meseci';
+                } elseif ($ageInMonths >= 5) {
+                    $age = $leta . $ageInMonths . ' mesecev';
+                } elseif ($ageInMonths == 0 && $ageInYears == 0) {
+                    $age = 'Manj kot 1 mesec.';
+                } else {
+                    $age = '';
+                }
+
+                if ($row['posvojen'] == 0) {
+                    $posvojen = 'Ne';
+                } else {
+                    $posvojen = 'Da';
+                }
+
+                if (is_null($row['rezervacija_id'])) {
+                    $rezervacija = "<a href='rezerviraj.php?zival_id=" . $row['id'] . "'>Rezerviraj</a>";
+                } else {
+                    $rezervacija = 'Rezervirano';
+                }
+
+                echo '<tr>';
+                echo '<td>' . $row['ime'] . "</td><td>" . $age . "</td><td>" . $posvojen . "</td><td>";
+
+                if (!empty($slika)) {
+                    echo "<img src='" . $slika . "'>";
+                } else {
+                    echo "Ni slike";
+                }
+
+                echo "</td><td>" . $rezervacija . "</td>";
+                echo '</tr>';
             }
+            ?>
+        </table>
+    </div>
 
-            $dateOfBirth = $row['datum_r'];
-            $today = date("Y-m-d");
-            $diff = date_diff(date_create($dateOfBirth), date_create($today));
-            $ageInMonths = $diff->format('%m');
-            $ageInYears = $diff->format('%y');
+    <div id="loginWindow" style="display: none;">
+        <?php if (isset($_COOKIE['prijava']) && $_COOKIE['good'] == 1) {
+            echo htmlspecialchars($_COOKIE['prijava']);
+            setcookie("prijava", "", time() - 3600);
+        } ?>
+    </div>
+    <div id="loginWindow2" style="display: none;">
+        <?php if (isset($_COOKIE['prijava']) && $_COOKIE['good'] != 1) {
+            echo htmlspecialchars($_COOKIE['prijava']);
+            setcookie("prijava", "", time() - 3600);
+        } ?>
+    </div>
 
-            if ($ageInYears == 1) {
-                $leta = $ageInYears . ' leto in ';
-            } elseif ($ageInYears > 1 && $ageInYears < 5) {
-                $leta = $ageInYears . ' leti in ';
-            } elseif ($ageInYears >= 5) {
-                $leta = $ageInYears . ' let in ';
-            } else {
-                $leta = '';
+    <script>
+        // Check if the cookie 'prijava' exists
+        function checkCookie() {
+            var name = "prijava=";
+            var decodedCookie = decodeURIComponent(document.cookie);
+            var cookieArray = decodedCookie.split(';');
+
+            for (var i = 0; i < cookieArray.length; i++) {
+                var cookie = cookieArray[i];
+                while (cookie.charAt(0) == ' ') {
+                    cookie = cookie.substring(1);
+                }
+                if (cookie.indexOf(name) == 0) {
+                    return true;
+                }
             }
-
-            if ($ageInMonths == 1) {
-                $age = $leta . '1 mesec';
-            } elseif ($ageInMonths > 1 && $ageInMonths < 5) {
-                $age = $leta . $ageInMonths . ' meseci';
-            } elseif ($ageInMonths >= 5) {
-                $age = $leta . $ageInMonths . ' mesecev';
-            } elseif ($ageInMonths == 0 && $ageInYears == 0) {
-                $age = 'Manj kot 1 mesec.';
-            } else {
-                $age = '';
-            }
-
-            if ($row['posvojen'] == 0) {
-                $posvojen = 'Ne';
-            } else {
-                $posvojen = 'Da';
-            }
-
-            if (is_null($row['rezervacija_id'])) {
-                $rezervacija = "<a href='rezerviraj.php?zival_id=".$row['id']."'>Rezerviraj</a>";
-            } else {
-                $rezervacija = 'Rezervirano';
-            }
-
-            echo '<tr>';
-            echo '<td>'.$row['ime']."</td><td>".$age."</td><td>".$posvojen."</td><td>";
-
-            if (!empty($slika)) {
-                echo "<img src='".$slika."'>";
-            } else {
-                echo "Ni slike";
-            }
-
-            echo "</td><td>".$rezervacija."</td>";
-            echo '</tr>';
+            return false;
         }
-        ?>
-    </table>
-</div>
 
-<div id="loginWindow" style="display: none;">
-    <?php if(isset($_COOKIE['prijava']) && $_COOKIE['good'] == 1){
-        echo htmlspecialchars($_COOKIE['prijava']);
-        setcookie("prijava", "", time() - 3600);
-    }?>
-</div>
-<div id="loginWindow2" style="display: none;">
-    <?php if(isset($_COOKIE['prijava']) && $_COOKIE['good'] != 1){
-        echo htmlspecialchars($_COOKIE['prijava']);
-        setcookie("prijava", "", time() - 3600);
-    }?>
-</div>
-
-<script>
-    // Check if the cookie 'prijava' exists
-    function checkCookie() {
-        var name = "prijava=";
-        var decodedCookie = decodeURIComponent(document.cookie);
-        var cookieArray = decodedCookie.split(';');
-
-        for (var i = 0; i < cookieArray.length; i++) {
-            var cookie = cookieArray[i];
-            while (cookie.charAt(0) == ' ') {
-                cookie = cookie.substring(1);
-            }
-            if (cookie.indexOf(name) == 0) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    // Show the appropriate login window based on the cookie value
-    if (checkCookie()) {
-        var loginWindow = document.getElementById("loginWindow");
-        var loginWindow2 = document.getElementById("loginWindow2");
-
-        if (document.cookie.indexOf("good=1") !== -1) {
+        // Show the appropriate login window based on the cookie value
+        if (checkCookie()) {
+            var loginWindow = document.getElementById("loginWindow");
             loginWindow.style.display = "block";
-            setTimeout(function() {
-                loginWindow.style.display = "none";
-            }, 5000);
         } else {
+            var loginWindow2 = document.getElementById("loginWindow2");
             loginWindow2.style.display = "block";
-            setTimeout(function() {
-                loginWindow2.style.display = "none";
-            }, 5000);
         }
-    }
 
+        // Toggle the dropdown menu
+        function myFunction(x) {
+            x.classList.toggle("change");
+            var dropdownMenu = document.getElementById("dropdown-menu");
+            if (dropdownMenu.style.display === "none") {
+                dropdownMenu.style.display = "block";
+            } else {
+                dropdownMenu.style.display = "none";
+            }
+        }
+    </script>
+</body>
+</html>
