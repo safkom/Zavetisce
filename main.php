6139f0032554374295f294305d064c51f055949d
Miha Šafranko <miha.safranko@gmail.com>
2023-05-29 20:20:14 +0200

    asd

--- main.php
+++ main.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Miha Šafranko"/>
-    <meta name="author" content="Miha Šafranko" />
-    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <meta name="description" content="Miha Šafranko">
+    <meta name="author" content="Miha Šafranko">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
     <title>Zavetišče</title>
     <style>
         
@@ -15,25 +15,27 @@
 <?php
 require_once 'cookie.php';
 require_once 'connect.php';
-if(!isset($_COOKIE['id'])){
+if (!isset($_COOKIE['id'])) {
     header('Location: index.php');
     exit();
 }
-if(isset($_COOKIE['admin'])){
+if (isset($_COOKIE['admin'])) {
     header('Location: admin.php');
     exit();
 }
 $id = $_COOKIE['id'];
+// Sanitize the user input to prevent SQL injection
+$id = mysqli_real_escape_string($conn, $id);
 $sql = "SELECT * FROM uporabniki WHERE id = $id;";
-$result = mysqli_query($conn,$sql);
+$result = mysqli_query($conn, $sql);
 $query = mysqli_num_rows($result);
-// modify the if statement to check if id exists in database
-if($query == 0){
+// Modify the if statement to check if id exists in the database
+if ($query == 0) {
     header('Location: index.php');
     exit();
 }
 $sql = "SELECT * FROM zivali;";
-$result = mysqli_query($conn,$sql);
+$result = mysqli_query($conn, $sql);
 echo "<a href='rezervacije.php'>Rezervacije</a><br>";
 echo "Seznam kužkov:";
 echo '<table border="1">';
@@ -41,10 +43,10 @@ echo '<tr>';
 echo "<td><b>Ime</b></td><td><b>Starost</b></td><td><b>Posvojen</b></td><td><b>Slika</b></td><td><b>Rezerviraj</b></td>";
 echo '</tr>';
 
-while($row = mysqli_fetch_array($result)){
+while ($row = mysqli_fetch_array($result)) {
     $slikaid = $row['slika_id'];
-    $sql1 = "SELECT * FROM slike WHERE id = ".$slikaid.";";
-    $klic = mysqli_query($conn,$sql1);
+    $sql1 = "SELECT * FROM slike WHERE id = '$slikaid';";
+    $klic = mysqli_query($conn, $sql1);
     $klic1 = mysqli_fetch_array($klic);
     $slika = $klic1['url'];
 
@@ -53,62 +55,44 @@ while($row = mysqli_fetch_array($result)){
     $diff = date_diff(date_create($dateOfBirth), date_create($today));
     $ageInMonths = $diff->format('%m');
     $ageInYears = $diff->format('%y');
-    if($ageInYears == 1){
+    if ($ageInYears == 1) {
         $leta = $ageInYears. ' leto in ';
-    }
-    elseif($ageInYears == 2){
+    } elseif ($ageInYears == 2) {
         $leta = $ageInYears. ' leti in ';
-    }
-    elseif($ageInYears == 3){
+    } elseif ($ageInYears == 3 || $ageInYears == 4) {
         $leta = $ageInYears. ' leta in ';
-    }
-    elseif($ageInYears == 4){
-        $leta = $ageInYears. ' leta in ';
-    }
-    elseif($ageInYears > 4){
+    } elseif ($ageInYears > 4) {
         $leta = $ageInYears. ' let in ';
-    }
-    else{
+    } else {
         $leta = '';
     }
 
-    if($ageInMonths == 1){
+    if ($ageInMonths == 1) {
         $age = $leta. '1 mesec';
-    }
-    elseif($ageInMonths == 2){
-        $age = $leta. '2 mesca';
-    }
-    elseif($ageInMonths == 3){
-        $age = $leta. '3 meseci';
-    }
-    elseif($ageInMonths == 4){
-        $age = $leta . '4 meseci';
-    }
-    elseif($ageInMonths > 4){
+    } elseif ($ageInMonths == 2 || $ageInMonths == 3 || $ageInMonths == 4) {
+        $age = $leta. $ageInMonths . ' meseci';
+    } elseif ($ageInMonths > 4) {
         $age = $leta . $ageInMonths . ' mescov';
-    }
-    else{
+    } else {
         $age = '';
     }
 
-    if($row['posvojen'] == 0){
+    if ($row['posvojen'] == 0) {
         $posvojen = 'Ne';
-    }
-    else{
+    } else {
         $posvojen = 'Da';
     }
 
-    if(is_null($row['rezervacija_id'])){
+    if (is_null($row['rezervacija_id'])) {
         $rezervacija = "<a href='rezerviraj.php?zival_id=".$row['id']."'>Rezerviraj</a>";
-    }
-    else{
+    } else {
         $rezervacija = 'Rezervirano';
     }
 
     echo '<tr>';
     echo '<td>'.$row['ime']."</td><td>".$age."</td><td>".$posvojen."</td><td>";
     
-    if ($slika !== null) {
+    if (!empty($slika)) {
         echo "<img src='".$slika."'>";
     } else {
         echo "Ni slike";
@@ -121,14 +105,14 @@ echo '</table>';
 ?>
 
 <div id="loginWindow" style="display: none;">
-    <?php if(isset($_COOKIE['prijava']) && $_COOKIE['good'] == 1){
-        echo $_COOKIE['prijava'];
+    <?php if (isset($_COOKIE['prijava']) && $_COOKIE['good'] == 1) {
+        echo htmlspecialchars($_COOKIE['prijava']);
         setcookie("prijava", "", time() - 3600);
     }?>
 </div>
 <div id="loginWindow2" style="display: none;">
-    <?php if(isset($_COOKIE['prijava']) && $_COOKIE['good'] != 1){
-        echo $_COOKIE['prijava'];
+    <?php if (isset($_COOKIE['prijava']) && $_COOKIE['good'] != 1) {
+        echo htmlspecialchars($_COOKIE['prijava']);
         setcookie("prijava", "", time() - 3600);
     }?>
 </div>
@@ -172,7 +156,7 @@ echo '</table>';
         }
     }
     
-    document.cookie = 'prijava=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
+    document.cookie = 'prijava=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
 </script>
 
 </body>
