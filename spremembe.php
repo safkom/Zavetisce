3b27b2a28f62848aa739916a2ae1b74ece58e161
Miha Šafranko <miha.safranko@gmail.com>
2023-05-29 22:42:19 +0200

    as

--- spremembe.php
+++ spremembe.php
@@ -77,18 +77,7 @@
     setcookie('register', '');
     ?>
 </datalist>
-				</div>
-				<button class="button login__submit">
-					<span class="button__text">Pošlji</span>
-					<i class="button__icon fas fa-chevron-right"></i>
-				</button>				
-			</form>
-		</div>
-		<div class="screen__background">
-			<span class="screen__background__shape screen__background__shape4"></span>
-			<span class="screen__background__shape screen__background__shape3"></span>		
-			<span class="screen__background__shape screen__background__shape2"></span>
-			<span class="screen__background__shape screen__background__shape1"></span>
-		</div>		
-	</div>
+<input type="submit" value="Pošlji">
 </div>
+</body>
+</html>
