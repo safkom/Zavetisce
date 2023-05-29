8f78c708c660c45d18d71539c6c262836bf31138
Miha Šafranko <miha.safranko@gmail.com>
2023-05-29 23:07:02 +0200

    asd

--- rezervacije.php
+++ rezervacije.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Miha Šafranko"/>
-	<meta name="author" content="Miha Šafranko" />
+    <meta name="author" content="Miha Šafranko" />
     <link rel="stylesheet" type="text/css" href="css/rezervacije.css" />
     <style>
         body{
@@ -19,85 +19,74 @@ require_once 'cookie.php';
 require_once 'connect.php';
 $id = $_COOKIE['id'];
 $sql = "SELECT * FROM uporabniki WHERE id = $id;";
-$result = mysqli_query($conn,$sql);
+$result = mysqli_query($conn, $sql);
 $query = mysqli_num_rows($result);
-// modify the if statement to check if id exists in database
-if($query > 0){
-    if(isset($_COOKIE['prijava'])){
-        echo $_COOKIE['prijava']."<br>";
-    }   
-setcookie('prijava', '');
-$sql = "SELECT * FROM zivali INNER JOIN rezervacija on zivali.id = rezervacija.zival_id WHERE rezervacija.uporabnik_id = ".$id.";";
-$result = mysqli_query($conn,$sql);
-echo "Seznam kužkov:";
-    echo '<table border ="1">';
-    echo'<tr>';
-        echo "<td><b>Ime</b></td><td><b>Starost</b></td><td><b>Posvojen</b></td><td><b>Slika</b></td><td><b>Preklici rezervacijo</b></td>";
-        echo'</tr>';
-
-    while($row=mysqli_fetch_array($result)){
+// modify the if statement to check if id exists in the database
+if ($query > 0) {
+    if (isset($_COOKIE['prijava'])) {
+        echo $_COOKIE['prijava'] . "<br>";
+    }
+    setcookie('prijava', '');
+    $sql = "SELECT * FROM zivali INNER JOIN rezervacija ON zivali.id = rezervacija.zival_id WHERE rezervacija.uporabnik_id = " . $id . ";";
+    $result = mysqli_query($conn, $sql);
+    echo "Seznam kužkov:";
+    echo '<table border="1">';
+    echo '<tr>';
+    echo "<td><b>Ime</b></td><td><b>Starost</b></td><td><b>Posvojen</b></td><td><b>Slika</b></td><td><b>Preklici rezervacijo</b></td>";
+    echo '</tr>';
+    while ($row = mysqli_fetch_array($result)) {
         $slikaid = $row['slika_id'];
-        $sql = "SELECT * FROM slike WHERE id = ".$slikaid.";";
-        $klic = mysqli_query($conn,$sql);
+        $sql = "SELECT * FROM slike WHERE id = " . $slikaid . ";";
+        $klic = mysqli_query($conn, $sql);
         $klic1 = mysqli_fetch_array($klic);
         $slika = $klic1['url'];
-
+    
         $dateOfBirth = $row['datum_r'];
         $today = date("Y-m-d");
         $diff = date_diff(date_create($dateOfBirth), date_create($today));
         $ageInMonths = $diff->format('%m');
         $ageInYears = $diff->format('%y');
-        if($ageInYears == 1){
-            $leta = $ageInYears. ' leto in ';
+        if ($ageInYears == 1) {
+            $leta = $ageInYears . ' leto in ';
+        } elseif ($ageInYears == 2) {
+            $leta = $ageInYears . ' leti in ';
+        } elseif ($ageInYears == 3) {
+            $leta = $ageInYears . ' leta in ';
+        } elseif ($ageInYears == 4) {
+            $leta = $ageInYears . ' leta in ';
+        } elseif ($ageInYears > 4) {
+            $leta = $ageInYears . ' let in ';
         }
-        if($ageInYears == 2){
-            $leta = $ageInYears. ' leti in ';
-        }
-        if($ageInYears == 3){
-            $leta = $ageInYears. ' leta in ';
-        }
-        if($ageInYears == 4){
-            $leta = $ageInYears. ' leta in ';
-        }
-        if($ageInYears > 4){
-            $leta = $ageInYears. ' let in ';
-        }
-        if($ageInMonths == 1){
-            $age = $leta. '1 mesec';
-        }
-        if($ageInMonths == 2){
-            $age = $leta. '2 mesca';
-        }
-        if($ageInMonths == 3){
-            $age = $leta. '3 meseci';
-        }
-        if($ageInMonths == 4){
+        if ($ageInMonths == 1) {
+            $age = $leta . '1 mesec';
+        } elseif ($ageInMonths == 2) {
+            $age = $leta . '2 mesca';
+        } elseif ($ageInMonths == 3) {
+            $age = $leta . '3 meseci';
+        } elseif ($ageInMonths == 4) {
             $age = $leta . '4 meseci';
-        }
-        if($ageInMonths > 4){
+        } elseif ($ageInMonths > 4) {
             $age = $leta . $ageInMonths . ' mescov';
-        }
-        if($ageInMonths < 1){
+        } elseif ($ageInMonths < 1) {
             $age = "Mlajši od 1 meseca.";
         }
-
-        if($row['posvojen'] == 0){
+    
+        if ($row['posvojen'] == 0) {
             $posvojen = 'Ne';
-        }
-        else{
+        } else {
             $posvojen = 'Da';
         }
-
-        echo'<tr>';
-        echo '<td>'.$row['ime']."</td><td> ".$age. "</td><td> ".$posvojen."</td><td><img src='".$slika."'></td><td><a href = 'preklici.php?zival_id=".$row['zival_id']."' >Preklici</td>";
-        echo'</tr>';
+    
+        echo '<tr>';
+        echo '<td>' . $row['ime'] . "</td><td> " . $age . "</td><td> " . $posvojen . "</td><td><img src='" . $slika . "'></td><td><a href='preklici.php?zival_id=" . $row['zival_id'] . "'>Preklici</td>";
+        echo '</tr>';
     }
     echo '</table>';
-}
-else{
+} else {
     header('Location: index.php');
-}
-?>
-<a href = "main.php">Nazaj</a>
-</body>
-</html>
+    }
+    ?>
+    <a href="main.php">Nazaj</a>
+    
+    </body>
+    </html>    
