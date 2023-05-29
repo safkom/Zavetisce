91558a42516e821a838008a04a336453d9d112d7
Miha Šafranko <miha.safranko@gmail.com>
2023-05-29 21:27:25 +0200

    d

--- registracija.php
+++ registracija.php
@@ -90,7 +90,7 @@ body {
   }
   ?>
   <div class = "container">
-    <h1>Registracija<h1>
+    <h1>Registracija</h1>
  <form action="register.php" method="get">
   <label for="ime">Ime:</label>
   <input type="text" id="ime" name="ime" required><br><br>
