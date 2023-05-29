b204704a289128f006326e5ff312b8dba977878b
Miha Šafranko <miha.safranko@gmail.com>
2023-05-29 20:55:51 +0200

    cvb

--- index.php
+++ index.php
@@ -19,12 +19,14 @@ body {
   }
   
   .container {
-    max-width: 400px;
-    margin: 50px auto;
-    padding: 20px;
-    background-color: #fff;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  }
+  max-width: 400px;
+  margin: 50px auto;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 50%; /* Adding rounded corners */
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2); /* Increasing the box shadow */
+}
+
   
   h1 {
     text-align: center;
