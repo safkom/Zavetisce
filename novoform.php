5701f3ea991720a37a93705529f368b2716b4cc7
StonePlex <miha.safranko@gmail.com>
2023-05-31 12:06:35 +0200

    asd

--- novoform.php
+++ novoform.php
@@ -4,78 +4,106 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Miha Šafranko"/>
-	<meta name="author" content="Miha Šafranko" />
-	<style>
+    <meta name="author" content="Miha Šafranko" />
+    <link rel="stylesheet" href="css/login.css">
+    <title>Spremembe</title>
+    <style>
         body{
             background: linear-gradient(90deg, #C7C5F4, #776BCC);
         }
-    </style>
-    <title>Vnos</title>
+        .container {
+  max-width: 400px;
+  margin: 50px auto;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 10px; /* Adding rounded corners */
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2); /* Increasing the box shadow */
+}
+
+
+  
+  h1 {
+    text-align: center;
+    color: #333;
+  }
+  
+  form {
+    margin-top: 20px;
+  }
+  
+  label {
+    display: block;
+    margin-bottom: 5px;
+    color: #555;
+  }
+  
+  .container input[type="text"],
+  .container input[type="date"],
+  .container input[type="file"],
+  .container input[type="datalist"],
+.container input[type="password"] {
+  width: 100%;
+  box-sizing: border-box;
+}
+
+.container input[type="submit"] {
+  width: 100%;
+  padding: 10px;
+  background-color: blue;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.container input[type="submit"]:hover {
+  background-color: lightblue;
+}/* Responsive Styles */
+@media (max-width: 600px) {
+    .container {
+      max-width: 100%;
+      margin: 20px;
+      box-shadow: none;
+    }
+  }
+        </style>
 </head>
 <body>
-  <?php
-  require_once 'cookie.php';
-  require_once 'connect.php'; ?>
-<div class="container">
-	<div class="screen">
-		<div class="screen__content">
-			<form class="login" id = "spremembe" action="novo.php" method="post" enctype="multipart/form-data">
-				<div class="login__field">
-					<i class="login__icon fas fa-user"></i>
-					<label for="ime">Ime:</label><br>
-                    <input type="text" id="ime" name="ime" required><br>
-				</div>
-				<div class="login__field">
-					<i class="login__icon fas fa-lock"></i>
-					<label for="datum">Datum rojstva:</label><br>
-                    <input type="date" id="datum" name="datum" class = login_input  required><br>
-				</div>
-                <div class="login__field">
-					<i class="login__icon fas fa-lock"></i>
-					<label for="posvojen">Posvojen:</label><br>
-                    <input type="checkbox" id="posvojen" name="posvojen" class="login_input" ><br>
-				</div>
-                <div class="login__field">
-					<i class="login__icon fas fa-lock"></i>
-					<label for="slika">Slika:</label><br>
-                    <input type="file" id="slika" class="login_input" name="slika"><br>
-				</div>
-
-<br>
-  <datalist id="zivali">
     <?php
+    require_once 'cookie.php';
     require_once 'connect.php';
-    $sql = "SELECT ime FROM zivali";
+    $zival = $_GET['zival_id'];
+    setcookie('zival_id', $zival);
+    $sql = "SELECT * FROM zivali WHERE id = ".$_GET['zival_id'].";";
     $result = mysqli_query($conn, $sql);
-    while ($row = mysqli_fetch_assoc($result)) {
-      echo "<option value = ".$row['ime'].">";
-    }
-    setcookie('register', '');
+    $row = mysqli_fetch_array($result);
     ?>
-</datalist>
-<datalist id = "uporabniki">
-<?php
-    require_once 'connect.php';
-    $sql = "SELECT * FROM uporabniki";
-    $result = mysqli_query($conn, $sql);
-    while ($row = mysqli_fetch_assoc($result)) {
-      echo "<option value = ".$row['email'].">";
-    }
-    setcookie('register', '');
-    ?>
-</datalist>
-				</div>
-				<button class="button login__submit">
-					<span class="button__text">Pošlji</span>
-					<i class="button__icon fas fa-chevron-right"></i>
-				</button>				
-			</form>
-		</div>
-		<div class="screen__background">
-			<span class="screen__background__shape screen__background__shape4"></span>
-			<span class="screen__background__shape screen__background__shape3"></span>		
-			<span class="screen__background__shape screen__background__shape2"></span>
-			<span class="screen__background__shape screen__background__shape1"></span>
-		</div>		
-	</div>
-</div>
+    <div class="container">
+        <h1>Spremembe</h1>
+        <form class="login" id="spremembe" action="novo.php" method="post" enctype="multipart/form-data">
+            <label for="ime">Ime:</label>
+            <input type="text" id="ime" name="ime"  required><br><br>
+
+            <label for="datum">Datum rojstva:</label>
+            <input type="date" id="datum" name="datum" class="login_input"  required><br><br>
+
+            <label for="posvojen">Posvojen:</label>
+            <input type="checkbox" id="posvojen" name="posvojen" class="login_input"><br><br>
+
+            <label for="slika">Slika:</label>
+            <input type="file" id="slika" class="login_input" name="slika"><br><br>
+
+            <br>
+            <input type="submit" value="Pošlji">
+        </form>
+        <form class="delete" id ="delete" action="admin.php" method="post" enctype="multipart/form-data">
+        <input type="submit" value="Nazaj" style = "width: 100%;
+  padding: 10px;
+  background-color: red;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;">
+    </div>
+</body>
+</html>
