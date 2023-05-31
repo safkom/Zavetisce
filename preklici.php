8d457e357fb7cca767c935cb8a1bd8a1196591d0
StonePlex <miha.safranko@gmail.com>
2023-05-31 12:01:09 +0200

    sql injection fix

--- preklici.php
+++ preklici.php
@@ -1,30 +1,38 @@
 <?php
 require_once 'connect.php';
 require_once 'cookie.php';
-    $id = $_COOKIE['id'];
-    $zival = $_GET['zival_id'];
-    echo $id;
-    echo "<br>";
-    echo $zival;
-    if(isset($_COOKIE['admin'])){
-        $sql = "DELETE FROM rezervacija WHERE zival_id = ".$zival.";";
-        if($conn->query($sql) === TRUE) {
-            setcookie('prijava', "Preklic uspešen.");
-            header('Location: admin.php');
-        }
-        else{
-            setcookie('prijava', "Error: " . $sql . "<br>" . $conn->error);
-            header('Location: admin.php');
-        }
-        exit();
-    }
-    $sql = "DELETE FROM rezervacija WHERE uporabnik_id = ".$id." AND zival_id = ".$zival.";";
-    if($conn->query($sql) === TRUE) {
+
+$id = $_COOKIE['id'];
+$zival = $_GET['zival_id'];
+echo $id;
+echo "<br>";
+echo $zival;
+
+if (isset($_COOKIE['admin'])) {
+    $sql = "DELETE FROM rezervacija WHERE zival_id = ?;";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $zival);
+    if ($stmt->execute()) {
         setcookie('prijava', "Preklic uspešen.");
-        header('Location: main.php');
+        header('Location: admin.php');
+    } else {
+        setcookie('prijava', "Error: " . $stmt->error);
+        header('Location: admin.php');
     }
-    else{
-        setcookie('prijava', "Error: " . $sql . "<br>" . $conn->error);
-        header('Location: main.php');
-    }
+    exit();
+}
+
+$sql = "DELETE FROM rezervacija WHERE uporabnik_id = ? AND zival_id = ?;";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ii", $id, $zival);
+if ($stmt->execute()) {
+    setcookie('prijava', "Preklic uspešen.");
+    header('Location: main.php');
+} else {
+    setcookie('prijava', "Error: " . $stmt->error);
+    header('Location: main.php');
+}
+
+$stmt->close();
+$conn->close();
 ?>
