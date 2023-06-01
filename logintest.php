f1c6076a6f553ae3c9ce182b279319cd6552aa8d
Miha Šafranko <miha.safranko@gmail.com>
2023-06-01 15:40:05 +0200

    gsdf

--- logintest.php
+++ logintest.php
@@ -48,8 +48,7 @@ body {
   .container input[type="text"],
 .container input[type="password"] {
   width: 360px;
-		background: #fff;
-		color: gray;
+		background: gray;
 		font: inherit;
 		box-shadow: 0 6px 10px 0 rgba(0, 0, 0 , .1);
 		border: 0;
