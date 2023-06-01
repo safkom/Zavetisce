6e87f830abb9671b1b61ad4c49bf7bb798741b12
Miha Šafranko <miha.safranko@gmail.com>
2023-06-01 15:38:50 +0200

    asd

--- logintest.php
+++ logintest.php
@@ -1,60 +1,25 @@
+<!DOCTYPE html>
+<html lang="sl">
 <head>
-<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-<style>
-    *,
-*::after,
-*::before {
-  margin: 0;
-  padding: 0;
-  box-sizing: inherit;
-  font-size: 62,5%;
-}
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Miha Šafranko"/>
+	<meta name="author" content="Miha Šafranko" />
+    <link rel="stylesheet" href="css/login.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <style>
+        body{
+            background: linear-gradient(90deg, #C7C5F4, #776BCC);
+        }
+        /* Global Styles */
 body {
-  height: 100vh;
-	width: 100%;
-  background: #0f2027; /* fallback for old browsers */
-  background: linear-gradient(90deg, #C7C5F4, #776BCC);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  color: #fff;
-}
-
-.form__label {
-  font-family: 'Roboto', sans-serif;
-  font-size: 1.2rem;
-  margin-left: 2rem;
-  margin-top: 0.7rem;
-  display: block;
-  transition: all 0.3s;
-  transform: translateY(0rem);
-  color: black;
-}
-
-.form__input {
-  font-family: 'Roboto', sans-serif;
-  color: #333;
-  font-size: 1.2rem;
-	margin: 0 auto;
-  padding: 1.5rem 2rem;
-  border-radius: 0.2rem;
-  background-color: rgb(255, 255, 255);
-  border: none;
-  width: 70%;
-  display: block;
-  border-bottom: 0.3rem solid transparent;
-  transition: all 0.3s;
-}
-
-.form__input:placeholder-shown + .form__label {
-  opacity: 0;
-  visibility: hidden;
-  -webkit-transform: translateY(-4rem);
-  transform: translateY(-4rem);
-}
-.container {
+    font-family: Arial, sans-serif;
+    background: linear-gradient(90deg, #C7C5F4, #776BCC);	
+    margin: 0;
+    padding: 0;
+  }
+  
+  .container {
   max-width: 400px;
   margin: 50px auto;
   padding: 20px;
@@ -62,6 +27,36 @@ body {
   border-radius: 10px; /* Adding rounded corners */
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.2); /* Increasing the box shadow */
 }
+
+
+  
+  h1 {
+    text-align: center;
+    color: #333;
+  }
+  
+  form {
+    margin-top: 20px;
+  }
+  
+  label {
+    display: block;
+    margin-bottom: 5px;
+    color: #555;
+  }
+  
+  .container input[type="text"],
+.container input[type="password"] {
+  width: 360px;
+		background: #fff;
+		color: $input-text-color;
+		font: inherit;
+		box-shadow: 0 6px 10px 0 rgba(0, 0, 0 , .1);
+		border: 0;
+		outline: 0;
+		padding: 22px 18px;
+}
+
 .container input[type="submit"] {
   width: 100%;
   padding: 10px;
@@ -75,28 +70,74 @@ body {
 .container input[type="submit"]:hover {
   background-color: lightblue;
 }
-a {
+  
+  a {
     color: lightblue;
     text-decoration: none;
   }
-#register{
-    color: black;
-}
-
-
+  
+  /* Responsive Styles */
+  @media (max-width: 600px) {
+    .container {
+      max-width: 100%;
+      margin: 20px;
+      box-shadow: none;
+    }
+  }
+  
     </style>
+
+    <title>Zavetišče</title>
 </head>
 <body>
 <div class="container">
   <h1>Prijava</h1>
-<div class="form__group">
-    <label for="name" class="form__label">Mail</label><br>
-  <input type="text" class="form__input" id="name" placeholder="Mail" required="" />
-  <label for="name" class="form__label">Geslo</label><br>
-  <input type="text" class="form__input" id="name" placeholder="Geslo" required="" />
-  
-  <input type="submit" value="Pošlji"><br>
-  <p id = "register">Niste še uporabnik? <a href = "registracija.php">Pridobite dostop</a>
+ <form action="preveri.php" method="get">
+  <label for="fname">Mail:</label>
+  <input type="text" id="email" name="email"><br><br>
+  <label for="lname">Geslo:</label>
+  <input type="password" id="geslo" name="geslo"><br><br>
+  <input type="submit" value="Pošlji">
+</form>
+<p>Niste še uporabnik? <a href = "registracija.php">Pridobite dostop </a>
 </div>
+
+<div id="loginWindow">
+    <?php
+    if (isset($_COOKIE['prijava'])) {
+        echo "✅ ";
+        echo $_COOKIE['prijava'];
+        // setcookie("prijava", "", time() - 3600);
+    }
+    ?>
 </div>
+<script>
+function checkCookie() {
+        var name = "prijava=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var cookieArray = decodedCookie.split(';');
+
+        for (var i = 0; i < cookieArray.length; i++) {
+            var cookie = cookieArray[i];
+            while (cookie.charAt(0) === ' ') {
+                cookie = cookie.substring(1);
+            }
+            if (cookie.indexOf(name) === 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Show the login window if the cookie exists
+    if (checkCookie()) {
+        var loginWindow = document.getElementById("loginWindow");
+        loginWindow.style.display = "block";
+        setTimeout(function () {
+            loginWindow.style.display = "none";
+        }, 5000);
+    }
+    document.cookie = 'prijava=; Max-Age=0';
+</script>
 </body>
+</html>
