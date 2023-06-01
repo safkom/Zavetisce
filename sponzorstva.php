e31e80c814a6cb955135a4997123cf863b85ce77
Miha Šafranko <miha.safranko@gmail.com>
2023-06-01 16:11:40 +0200

    sdf

--- sponzorstva.php
+++ sponzorstva.php
@@ -157,7 +157,7 @@ while ($row = mysqli_fetch_array($result)) {
     $sponzorstvo = ""; // Initialize the $sponzorstvo variable
     
     if (!is_null($row['sponzorstvo_id'])) {
-        $sponzorstvo = "<a href='preklicis.php?zival_id=".$row['s.id']."'>Prekliči</a>";
+        $sponzorstvo = "<a href='preklicis.php?zival_id=".$row['id']."'>Prekliči</a>";
     }
 
     echo "</td><td>".$sponzorstvo."</td>";
