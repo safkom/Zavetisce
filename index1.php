89695c9f36db300c484ecd92e01c38c091a96323
Miha Šafranko <miha.safranko@gmail.com>
2023-06-01 15:27:09 +0200

    asd

--- index1.php
+++ index1.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Miha Šafranko"/>
 	<meta name="author" content="Miha Šafranko" />
-    <link rel="stylesheet" href="css/index1.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <style>
         body{
@@ -26,19 +25,50 @@ body {
   background-color: #fff;
   border-radius: 10px; /* Adding rounded corners */
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.2); /* Increasing the box shadow */
+  }
+
+  .form__label {
+  font-family: 'Roboto', sans-serif;
+  font-size: 1.2rem;
+  margin-left: 2rem;
+  margin-top: 0.7rem;
+  display: block;
+  transition: all 0.3s;
+  transform: translateY(0rem);
+}
+.form__input {
+  font-family: 'Roboto', sans-serif;
+  color: #333;
+  font-size: 1.2rem;
+	margin: 0 auto;
+  padding: 1.5rem 2rem;
+  border-radius: 0.2rem;
+  background-color: rgb(255, 255, 255);
+  border: none;
+  width: 90%;
+  display: block;
+  border-bottom: 0.3rem solid transparent;
+  transition: all 0.3s;
+}
+
+.form__input:placeholder-shown + .form__label {
+  opacity: 0;
+  visibility: hidden;
+  -webkit-transform: translateY(-4rem);
+  transform: translateY(-4rem);
 }
     </style>
 
     <title>Zavetišče</title>
 </head>
 <body>
-<div class="form__group">
+<div class="container">
   <h1>Prijava</h1>
  <form action="preveri.php" method="get">
-  <label for="fname">Mail:</label>
-  <input type="text" id="email" name="email"><br><br>
-  <label for="lname">Geslo:</label>
-  <input type="password" id="geslo" name="geslo"><br><br>
+  <label for="fname" class="form__label">Mail:</label>
+  <input type="text" id="email" name="email" class="form__input" placeholder="Mail"><br><br>
+  <label for="lname" class="form__label">Geslo:</label>
+  <input type="password" id="geslo" name="geslo" class="form__input" placeholder="Geslo"><br><br>
   <input type="submit" value="Pošlji">
 </form>
 <p>Niste še uporabnik? <a href = "registracija.php">Pridobite dostop </a>
