54b8c280930f216e8defbb5db6955321d0da9238
StonePlex <miha.safranko@gmail.com>
2023-06-07 08:35:55 +0200

    asd

--- main.php
+++ main.php
@@ -287,54 +287,63 @@ $result = mysqli_query($conn, $sql);
 
 var filterOptionsContainer = document.getElementById("filterOptionsContainer");
 
-    function toggleFilterOptions() {
+function toggleFilterOptions() {
       if (filterOptionsContainer.style.display === "none") {
         filterOptionsContainer.style.display = "block";
       } else {
         filterOptionsContainer.style.display = "none";
       }
     }
+    
+function filterTable() {
+        // Get filter input values and convert them to lowercase
+        var filterNameValue = document.getElementById("filterName").value.toLowerCase();
+        var sortAgeValue = document.getElementById("sortAge").value;
 
-    function filterTable() {
-      var filterNameValue = document.getElementById("filterName").value.toLowerCase();
-      var sortAgeValue = document.getElementById("sortAge").value;
+        // Get all table rows
+        var rows = document.querySelectorAll("table tr");
 
-      var rows = document.querySelectorAll("#myTable tr");
+        // Loop through rows and hide those that don't match the filter
+        for (var i = 1; i < rows.length; i++) {
+          var name = rows[i].getElementsByTagName("td")[0].textContent.toLowerCase();
 
-      for (var i = 1; i < rows.length; i++) {
-        var name = rows[i].getElementsByTagName("td")[0].textContent.toLowerCase();
-
-        if (name.includes(filterNameValue)) {
-          rows[i].style.display = "";
-        } else {
-          rows[i].style.display = "none";
+          if (name.includes(filterNameValue)) {
+            rows[i].style.display = "";
+          } else {
+            rows[i].style.display = "none";
+          }
         }
+
+        // Sort table rows by age
+        if (sortAgeValue === "asc") {
+          var sortedRows = Array.from(rows).slice(1).sort(function(a, b) {
+            var ageA = parseInt(a.getElementsByTagName("td")[1].textContent);
+            var ageB = parseInt(b.getElementsByTagName("td")[1].textContent);
+            return ageA - ageB;
+          });
+        } else if (sortAgeValue === "desc") {
+          var sortedRows = Array.from(rows).slice(1).sort(function(a, b) {
+            var ageA = parseInt(a.getElementsByTagName("td")[1].textContent);
+            var ageB = parseInt(b.getElementsByTagName("td")[1].textContent);
+            return ageB - ageA;
+          });
+        } else {
+          var sortedRows = Array.from(rows).slice(1);
+        }
+
+        // Reorder table rows based on the sorted rows
+        var table = document.querySelector("table");
+        table.innerHTML = "";
+        table.appendChild(rows[0]); // Keep the table header row at the top
+
+        sortedRows.forEach(function(row) {
+          table.appendChild(row);
+        });
       }
 
-      if (sortAgeValue === "asc") {
-        var sortedRows = Array.from(rows).slice(1).sort(function(a, b) {
-          var ageA = parseInt(a.getElementsByTagName("td")[1].textContent);
-          var ageB = parseInt(b.getElementsByTagName("td")[1].textContent);
-          return ageA - ageB;
-        });
-      } else if (sortAgeValue === "desc") {
-        var sortedRows = Array.from(rows).slice(1).sort(function(a, b) {
-          var ageA = parseInt(a.getElementsByTagName("td")[1].textContent);
-          var ageB = parseInt(b.getElementsByTagName("td")[1].textContent);
-          return ageB - ageA;
-        });
-      } else {
-        var sortedRows = Array.from(rows).slice(1);
-      }
-
-      var table = document.getElementById("myTable");
-      table.innerHTML = "";
-      table.appendChild(rows[0]);
-
-      sortedRows.forEach(function(row) {
-        table.appendChild(row);
-      });
-    }
+      // Attach event listeners to the filter input fields
+      document.getElementById("filterName").addEventListener("input", filterTable);
+      document.getElementById("sortAge").addEventListener("change", filterTable);
 
 var menuBtn = document.getElementById("menuBtn");
     var menuContent = document.getElementById("menuContent");
