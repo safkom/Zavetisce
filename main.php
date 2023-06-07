30e9d357ff7fe77987899c74702efdefb1ebf037
StonePlex <miha.safranko@gmail.com>
2023-06-07 12:06:53 +0200

    as

--- main.php
+++ main.php
@@ -192,9 +192,6 @@ $result = mysqli_query($conn, $sql);
   <br>
   <div class="container">
 			<div class="row justify-content-center">
-				<div class="col-md-6 text-center mb-5">
-					<h2 class="heading-section">Table #02</h2>
-				</div>
 			</div>
 			<div class="row">
 				<div class="col-md-12">
